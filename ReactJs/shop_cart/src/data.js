64702d59456a55b1b98936357696d1bc1ea66c83
unknown <ahmedfaraaz73@gmail.com>
2022-01-11 18:44:32 +0530

    react shop

---
+++ ReactJs/shop_cart/src/data.js
@@ -0,0 +1,40 @@
+import img1 from './img/img1.png';
+import img2 from './img/img2.png';
+import img3 from './img/img3.png';
+import img4 from './img/img4.png';
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: 'Mens winter fleece jacket',
+            desc:'',
+            price: 199,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Mens vintage Jaacket',
+            desc:'',
+            price: 399,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'Black Printed Shirt',
+            desc:'',
+            price: 499,
+        },
+        {
+            id: 4,
+            img: img4,
+            title: 'Roaster Men Shirt',
+            desc:'',
+            price: 899,
+        }
+
+    ],
+};
+
+export default data;
